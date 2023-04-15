fdc13bc31e03bfdc95bc2e78e3a763f63a3e1f6e
Varad <120244476+Varad-02@users.noreply.github.com>
2023-04-16 01:59:45 +0530

    added

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-// if user chooses celsius to fahrenheit from radio buttons, celsius field enables and fahrenheit field disables
-function chooseCelsius() {
-    document.getElementById("celsius").disabled = false;
-    document.getElementById("fahrenheit").disabled = true;
-    // the unchosen field resets its value
-    document.getElementById("fahrenheit").value = "";
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('clear').onclick = clearForm;
+
+function tempConvert() {
+
+  var fahrenheit = document.getElementById("fahrenheit").value;
+  var celsius = document.getElementById("celsius").value;
+
+  if (fahrenheit != '') {
+    celsius = (parseFloat(fahrenheit) - 32) / 1.8;
+  } else {
+    fahrenheit = (parseFloat(celsius) * 1.8) + 32;
+
   }
-  // if user chooses fahrenheit to celsius from radio buttons, celsius field disables and fahrenheit field enables
-  function chooseFahrenheit() {
-    document.getElementById("celsius").disabled = true;
-    document.getElementById("fahrenheit").disabled = false;
-    // also the unchosed input field resets its value
-    document.getElementById("celsius").value = "";
-  }
-  // upon user input the result is calculated
-  function convertTemperatures() {
-    if (document.getElementById("celsius").disabled==false) {
-      document.getElementById("result").innerHTML = (document.getElementById("celsius").value*1.8+32).toFixed(1) + "&deg;F";
-    } else {
-      document.getElementById("result").innerHTML = ((document.getElementById("fahrenheit").value-32)/1.8).toFixed(1) + "&deg;C";
-    }
-  }
-  // if temperature is lower than absolute zero, error needs to be thrown at the user!
-  
-  window.onload = function() {
-    document.getElementById("c").onclick = chooseCelsius;
-    document.getElementById("f").onclick = chooseFahrenheit;
-    document.getElementById("celsius").oninput = convertTemperatures;
-    document.getElementById("fahrenheit").oninput = convertTemperatures;
-  }
+
+  document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
+  document.getElementById('celsius').value = parseFloat(celsius).toFixed(1);
+}
+
+function clearForm() {
+  document.getElementById('fahrenheit').value = '';
+  document.getElementById('celsius').value = '';
+}
